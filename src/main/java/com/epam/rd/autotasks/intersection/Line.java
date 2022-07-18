a48486c9f4f219d287b72e2333897ee67fa38595
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-18 19:29:42 +0300

    test 2

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -2,12 +2,21 @@ package com.epam.rd.autotasks.intersection;
 
 public class Line {
 
+     final int k, b;
+
     public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
 
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
-    }
+        if (k - other.k == 0) {
+            return null;
+        }
+        int x = (other.b - b) / (k - other.k);
+        int y = k * x + this.b;
 
+        return new Point(x, y);
+    }
 }
